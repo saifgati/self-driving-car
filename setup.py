e0fc7c9981cca84df97475096bf39b41cab3db3c
Gati Saif <50594108+saifgati@users.noreply.github.com>
2021-02-07 16:16:59 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='self-driving-project',
+    version='0.1',
+    description='Autonomous car in rasberry pi with tensorflow',
+    author='Paula Moraes, Felipe Salvatore',
+    packages=find_packages(),
+    test_suite="tests"
+)
